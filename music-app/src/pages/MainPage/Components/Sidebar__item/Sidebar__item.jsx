ab0b3bfd1cb80420d41f41abccecf4a83a8c9715
Rakhymzhan <mark.john.99@mail.ru>
2023-07-23 13:08:50 +0600

    fixed sidebar links

--- music-app/src/pages/MainPage/Components/Sidebar__item/Sidebar__item.jsx
+++ music-app/src/pages/MainPage/Components/Sidebar__item/Sidebar__item.jsx
@@ -4,7 +4,7 @@ import * as S from './styles';
 export const Sidebar__item = ({ img, description, loadingClass, link }) => {
   return (
     <S.Sidebar__item className={loadingClass}>
-      <Link>
+      <Link to={link}>
          <img src={img} alt={description} />
       </Link>
     </S.Sidebar__item>
